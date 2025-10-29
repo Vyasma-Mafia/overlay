d6cd6149278a92a7e71e52c2a76d85c47b811d51
mralex1810 <sash39av@gmail.com>
2025-10-29 17:24:27 +0300

    OVERLAY-12: fix

--- src/main/kotlin/com/stoum/overlay/entity/Game.kt
+++ src/main/kotlin/com/stoum/overlay/entity/Game.kt
@@ -13,11 +13,9 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OrderBy
 import jakarta.persistence.Table
-import jakarta.persistence.UniqueConstraint
 import jakarta.persistence.Version
 import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.type.SqlTypes
@@ -25,10 +23,7 @@ import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
-@Table(
-    name = "game",
-    uniqueConstraints = [UniqueConstraint(columnNames = ["tournamentId", "gameNum", "tableNum", "phase", "type"])]
-)
+@Table(name = "game")
 data class Game(
         @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -44,7 +39,6 @@ data class Game(
         @OrderBy("place ASC")
     var players: MutableList<GamePlayer> = mutableListOf(),
     @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
-    @JoinColumn(name = "game_id")
     var facts: MutableList<Fact> = mutableListOf(),
     var started: Boolean? = null,
     var visibleOverlay: Boolean? = true,
