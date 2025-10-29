05129ca90f643c9b2763a0f02b976967b7e976b9
mralex1810 <sash39av@gmail.com>
2025-10-29 17:19:56 +0300

    OVERLAY-12: fix

--- src/main/kotlin/com/stoum/overlay/entity/Game.kt
+++ src/main/kotlin/com/stoum/overlay/entity/Game.kt
@@ -13,6 +13,7 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OrderBy
 import jakarta.persistence.Table
